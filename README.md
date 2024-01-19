abedad6a6f3a1a72c64540c247ef41b6cda424ba
Gage Machado <gwm32@humboldt.edu>
2024-01-19 10:25:08 -0800

    Create symbolic link to vue-app

---
+++ README.md
@@ -0,0 +1,29 @@
+<<<<<<< HEAD
+# my-resume-website
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+=======
+# VuePractice
+>>>>>>> 2bbcd05da17d87645c91fd6c4e9c9b5b97e5ed4d
---
+++ src/views/HomeView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="home">
+    <div class="column">
+      <div id="about"><AboutMe /></div>
+      <div id="experience"><MyExperience /></div>
+    </div>
+    <div class="column">
+      <div id="education"><MyEducation /></div>
+      <div id="contact"><ContactMe /></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AboutMe from '@/components/AboutMe.vue';
+import MyExperience from '@/components/MyExperience.vue';
+import MyEducation from '@/components/MyEducation.vue';
+import ContactMe from '@/components/ContactMe.vue';
+
+export default {
+  components: {
+    AboutMe,
+    MyExperience,
+    MyEducation,
+    ContactMe
+  },
+};
+</script>
+
+<style>
+.home {
+  display: flex;
+  justify-content: space-between;
+  padding: 20px; 
+  margin: 0 auto; 
+  max-width: 1200px;
+  min-height: 100vh;
+  background-color: antiquewhite;
+}
+
+.column {
+  flex: 1;
+  margin: 0 10px;
+}
+</style>
